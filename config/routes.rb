2b583eb148738937c985bf4ed99238015c196af8
Peter West <source@petewest.org>
2013-12-05 18:59:46 +0000

    Correct routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Run::Application.routes.draw do
-  get "static_pages/about", as: :about
+  get "about" => "static_pages#about", as: :about
   root 'static_pages#about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
