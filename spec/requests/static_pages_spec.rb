251b6cf46ec03412e39bfb4000fc638661f48fae
Peter West <source@petewest.org>
2013-12-05 23:07:13 +0000

    Provide title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,4 +10,13 @@ describe "Static pages" do
     it {should have_title('Run') }
     it {should have_content('Run') }
   end
+  
+  describe "About page" do
+    
+    subject { page }
+    before { visit about_path }
+    
+    it { should have_title('About')}
+    it { should have_content('About') }
+  end
 end
