9cb95e7696e1d07b546b666f290f0ec2e2c91513
Peter West <source@petewest.org>
2013-12-05 16:36:31 +0000

    Changed routes a bit

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,7 +5,7 @@ describe "Static pages" do
   describe "About page" do
 
     subject { page }
-    before { visit '/static_pages/about' }
+    before { visit about_path }
 
     it {should have_title('Run') }
     it {should have_content('Run') }
