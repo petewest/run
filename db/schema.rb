e1833e068d189ba3f170253b84819dab60b1cefe
Peter West <source@petewest.org>
2013-12-06 22:10:25 +0000

    Basic signup page created

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131206202623) do
+ActiveRecord::Schema.define(version: 20131206215353) do
 
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.string   "gravatar_email"
-    t.boolean  "admin"
+    t.boolean  "admin",           default: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
