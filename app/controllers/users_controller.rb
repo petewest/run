dbd90138b1d3bb80b2c6e63543cf168fc774d7c4
Peter West <source@petewest.org>
2013-12-06 22:24:05 +0000

    Working user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,19 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success]="Welcome to the site, #{@user.name}!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :gravatar_email)
+  end
 end
