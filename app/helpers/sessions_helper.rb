c163f4d21e2e1165964289144b5b34245cad15ab
Peter West <source@petewest.org>
2013-12-07 19:12:25 +0000

    Always store remember_token in db so we can control all user logins later (after I move the session to its own model)

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,12 +3,13 @@ module SessionsHelper
     #hybrid auth section, will store user id in a session variable and a remmeber token in the db if the user ticked "remember"
     #this way we can use the session variable if it's there, or populate from the db if not
     #to give us a way to have persistent and semi-persistent sessions
-    session[:user_id]=user.id
-    #If we're to remember this user forever then store the permanent token in a cookie
-    if remember_forever
-      remember_token = User.new_remember_token
+    remember_token = User.new_remember_token
+    user.update_attribute(:remember_token, User.encrypt(remember_token))
+    unless remember_forever
+      session[:remember_token]=remember_token
+    else
+      #If we're to remember this user forever then store the permanent token in a cookie
       cookies.permanent[:remember_token] = remember_token
-      user.update_attribute(:remember_token, User.encrypt(remember_token))
     end
     #use setter function 
     self.current_user= user
@@ -23,10 +24,12 @@ module SessionsHelper
   
   #change default getter to lookup based on either session variable or cookie
   def current_user
-    if (!session[:user_id].nil?)
-      @current_user||=User.find(session[:user_id])
-    else
+    if (!session[:remember_token].nil?)
+      @current_user||=User.find_by(remember_token: User.encrypt(session[:remember_token]))
+    elsif (!cookies[:remember_token].nil?)
       @current_user||=User.find_by(remember_token: User.encrypt(cookies[:remember_token]))
     end
   end
+  
+  #Sign out
 end
