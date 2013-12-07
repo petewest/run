101bf5a1597a57962845415dda4dbb73dfa1b25f
Peter West <source@petewest.org>
2013-12-07 19:41:40 +0000

    Change remember token when user logs out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,11 +5,12 @@ module SessionsHelper
     #to give us a way to have persistent and semi-persistent sessions
     remember_token = User.new_remember_token
     user.update_attribute(:remember_token, User.encrypt(remember_token))
-    unless remember_forever
-      session[:remember_token]=remember_token
-    else
+    if remember_forever
       #If we're to remember this user forever then store the permanent token in a cookie
-      cookies.permanent[:remember_token] = remember_token
+      cookies.permanent[:remember_token] = remember_token      
+    else
+      #Otherwise we'll just store it for this session
+      session[:remember_token]=remember_token
     end
     #use setter function 
     self.current_user= user
@@ -24,12 +25,25 @@ module SessionsHelper
   
   #change default getter to lookup based on either session variable or cookie
   def current_user
-    if (!session[:remember_token].nil?)
+    if (session[:remember_token])
       @current_user||=User.find_by(remember_token: User.encrypt(session[:remember_token]))
-    elsif (!cookies[:remember_token].nil?)
+    elsif (cookies[:remember_token])
       @current_user||=User.find_by(remember_token: User.encrypt(cookies[:remember_token]))
     end
   end
   
   #Sign out
+  def sign_out
+    #Replace the remember_token (we'll change this later to delete the session item)
+    #Just incase someone does a nasty and restores any cookies
+    if signed_in?
+      remember_token = User.new_remember_token
+      self.current_user.update_attribute(:remember_token, User.encrypt(remember_token))
+    end
+    #Remove current_user
+    self.current_user=nil
+    #Tidy up any session vars or cookies
+    session.delete(:remember_token) if session[:remember_token]
+    cookies.delete(:remember_token) if cookies[:remember_token]
+  end
 end
