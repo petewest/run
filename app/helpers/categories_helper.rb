708340e7215328522f898a6e738105096d05d197
Peter West <source@petewest.org>
2013-12-11 23:41:43 +0000

    Still tidying that header

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -3,6 +3,6 @@ module CategoriesHelper
     @all_categories||=Category.all
   end
   def current_category
-    @current_category||=Category.find_by_stub(params[:category])
+    @current_category||=Category.find_by_stub(params[:category]) if params[:category]
   end
 end
