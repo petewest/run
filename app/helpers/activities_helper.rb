5571a36efa1e3e390030127a35e69713a3804d69
Peter West <source@petewest.org>
2013-12-15 22:03:15 +0000

    Changed map_for to be called map_for_activity and to take an optional hash of items to add (e.g. class:

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -2,8 +2,14 @@ module ActivitiesHelper
   def gmaps_api_key
     "AIzaSyBn59e0xUZcNRyxjJljFE0bHqMvHqSt4H4"
   end
-  def map_for(id)
+  
+  def map_for_activity(id, options={})
     activity=Activity.find(id)
-    "<img src='http://maps.googleapis.com/maps/api/staticmap?visual_refresh=true&maptype=terrain&size=600x400&path=weight:3%7Ccolor:red%7Cenc:#{activity.polyline}&sensor=false' />".html_safe if activity
+    if activity
+      url="http://maps.googleapis.com/maps/api/staticmap?visual_refresh=true&maptype=terrain&size=600x400&path=weight:3%7Ccolor:red%7Cenc:#{activity.polyline}&sensor=false"
+      opts=" "
+      options.each {|k,v| opts+="#{k.to_s}='#{v}' " }
+      "<img#{opts}src='#{url}' />".html_safe
+    end
   end
 end
