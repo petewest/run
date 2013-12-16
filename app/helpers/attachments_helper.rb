49f5d4dce124bb07f9782a11e84545f8d2064bb3
Peter West <source@petewest.org>
2013-12-16 23:58:45 +0000

    Add img tag to redcloth extender

--- app/helpers/attachments_helper.rb
+++ app/helpers/attachments_helper.rb
@@ -1,2 +1,11 @@
 module AttachmentsHelper
+  
+  def attachment_image(id)
+    begin
+      attachment=Attachment.find(id)
+    rescue ActiveRecord::RecordNotFound => e
+      return ""
+    end
+    %Q{<img src="#{attachment.file.url(:large)}" />}
+  end
 end
--- app/helpers/redcloth_extensions.rb
+++ app/helpers/redcloth_extensions.rb
@@ -1,5 +1,6 @@
 module RedclothExtensions
   include ActivitiesHelper
+  include AttachmentsHelper
   
   def map(opts)
     args=/\A(?<static>[s]?)(?<align>(&gt;|&lt;|\|)?)\s*(?<id>\d+)\s*(?<caption>.*)/.match(opts[:text])
@@ -13,6 +14,21 @@ module RedclothExtensions
     html=%Q{<div class="#{css}" id="map_container_#{args[:id]}">}
     html+=map_for_activity_redcloth(args[:id], args[:static].blank? ? false : true)
     html+=%Q{<div class="map_caption">#{args[:caption]}</div>} unless args[:caption].blank?
+    html+=%Q{</div>}
+  end
+  
+  def img(opts)
+    args=/\A(?<static>[s]?)(?<align>(&gt;|&lt;|\|)?)\s*(?<id>\d+)\s*(?<caption>.*)/.match(opts[:text])
+    return "" if args.nil?
+    css="post_image"
+    css+=case args[:align]
+      when "|" then " center"
+      when "&gt;" then " pull-right"
+      else " pull-left"
+    end
+    html=%Q{<div class="#{css}" id="image_container_#{args[:id]}">}
+    html+= attachment_image(args[:id])
+    html+=%Q{<div class="image_caption">#{args[:caption]}</div>} unless args[:caption].blank?
     html+=%Q{</div>}
   end
 end
