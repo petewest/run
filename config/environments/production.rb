6161c640f8d274a01b7b75981ca34f7cde5d8133
Peter West <source@petewest.org>
2013-12-16 16:37:37 +0000

    Add precompile for posts.js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Run::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-   config.assets.precompile += %w( activities.js )
+   config.assets.precompile += %w( activities.js posts.js)
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
