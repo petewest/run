84606b21c0027f54468f37ff3296560649447c5e
Peter West <source@petewest.org>
2013-12-17 00:01:40 +0000

    Add thumbnail for preview screen

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,7 +1,8 @@
 class Attachment < ActiveRecord::Base
   has_attached_file :file, styles: {
     medium:"300x300>",
-    large:"600x600>"
+    large:"600x600>",
+    thumb:"100x100>"
   }
   
   belongs_to :user
