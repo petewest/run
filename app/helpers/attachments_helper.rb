8c2825c30c50223ea4852785c90647738e9e1cb5
Peter West <source@petewest.org>
2013-12-19 00:51:56 +0000

    Refactoring code

--- app/helpers/attachments_helper.rb
+++ app/helpers/attachments_helper.rb
@@ -1,11 +1,11 @@
 module AttachmentsHelper
   
-  def attachment_image(id)
+  def attachment_image(id, size= :large)
     begin
       attachment=Attachment.find(id)
     rescue ActiveRecord::RecordNotFound => e
       return ""
     end
-    %Q{<img src="#{attachment.file.url(:large)}" />}
+    %Q{<img src="#{attachment.file.url(size)}" />}
   end
 end
