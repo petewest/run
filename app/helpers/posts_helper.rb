2c68e166f1dbb8705e98b461d04991174ab62892
Peter West <source@petewest.org>
2013-12-23 15:07:56 +0000

    Tidied summary code, made rss feed show summary

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,15 @@ module PostsHelper
   def writeup_break
     "[--BREAK--]"
   end
+  def write_up_summary(post, options = {})
+    omit=options.fetch(:omit, %Q{["... read more":#{url_for(post)}]})
+    split=post.write_up.split(writeup_break)
+    text=split.first
+    text+=omit if split.length>1
+    text
+  end
+  def write_up_detail(write_up)
+    write_up.gsub(writeup_break,"")
+  end
+
 end
