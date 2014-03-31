192ff49df2e13c843fe664b64a24b7d22ebc661d
Peter West <source@petewest.org>
2014-03-31 14:12:39 +0100

    Tidy

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140119110940) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "activities", force: true do |t|
     t.datetime "start_time"
     t.float    "distance"
